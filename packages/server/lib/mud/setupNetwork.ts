a6723a698b96072a9c59ab553b6df32494ff00aa
alvrs <alvarius@lattice.xyz>
2023-05-19 14:18:27 +0200

    revert formatting changes

--- packages/server/lib/mud/setupNetwork.ts
+++ packages/server/lib/mud/setupNetwork.ts
@@ -1,8 +1,4 @@
-import {
-  createFastTxExecutor,
-  createFaucetService,
-  getSnapSyncRecords,
-} from "@latticexyz/network";
+import { createFastTxExecutor, createFaucetService, getSnapSyncRecords } from "@latticexyz/network";
 import { getNetworkConfig } from "./getNetworkConfig";
 import { defineContractComponents } from "./contractComponents";
 import { world } from "./world";
@@ -13,24 +9,21 @@ import { getTableIds } from "@latticexyz/utils";
 import storeConfig from "contracts/mud.config";
 import { setupMUDV2Network } from "@latticexyz/std-client";
 
-const IWorld__factory = (worldFactory as any).default.IWorld__factory;
+const { IWorld__factory } = (worldFactory as any).default.IWorld__factory;
 
 export type SetupNetworkResult = Awaited<ReturnType<typeof setupNetwork>>;
 
 export async function setupNetwork() {
   const contractComponents = defineContractComponents(world);
   const networkConfig = await getNetworkConfig();
-  const result = await setupMUDV2Network<
-    typeof contractComponents,
-    typeof storeConfig
-  >({
+  const result = await setupMUDV2Network<typeof contractComponents, typeof storeConfig>({
     networkConfig,
     world,
     contractComponents,
     syncThread: "main",
     storeConfig,
     worldAbi: IWorld__factory.abi,
-    syncStoreCache: false,
+    syncStoreCache: false
   });
 
   // Request drip from faucet
@@ -61,10 +54,7 @@ export async function setupNetwork() {
   const provider = result.network.providers.get().json;
   const signerOrProvider = signer ?? provider;
   // Create a World contract instance
-  const worldContract = IWorld__factory.connect(
-    networkConfig.worldAddress,
-    signerOrProvider
-  );
+  const worldContract = IWorld__factory.connect(networkConfig.worldAddress, signerOrProvider);
 
   if (networkConfig.snapSync) {
     const currentBlockNumber = await provider.getBlockNumber();
@@ -84,9 +74,7 @@ export async function setupNetwork() {
   // Create a fast tx executor
   const fastTxExecutor =
     signer?.provider instanceof JsonRpcProvider
-      ? await createFastTxExecutor(
-          signer as Signer & { provider: JsonRpcProvider }
-        )
+      ? await createFastTxExecutor(signer as Signer & { provider: JsonRpcProvider })
       : null;
 
   // TODO: infer this from fastTxExecute signature?
@@ -98,9 +86,7 @@ export async function setupNetwork() {
     }
   ) => Promise<ReturnType<C[F]>>;
 
-  function bindFastTxExecute<C extends Contract>(
-    contract: C
-  ): BoundFastTxExecuteFn<C> {
+  function bindFastTxExecute<C extends Contract>(contract: C): BoundFastTxExecuteFn<C> {
     return async function (...args) {
       if (!fastTxExecutor) {
         throw new Error("no signer");
