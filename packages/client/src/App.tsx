13be297971f0fceccbc11e2aef5f7973c41aa6ed
Lance Terrence Magsombol <lance@oostvoort.com>
2023-05-19 18:59:57 +0800

    refactor: use updated game component

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,9 +1,10 @@
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {CreatePlayer} from "./pages/CreatePlayer";
-import GridStoryLayout from './components/templates/grid_story_layout'
+import CreatePlayerNew from './pages/create-player-new'
 import MainLayout from './components/templates/main_layout'
 import { Routes, Route } from 'react-router-dom'
 import Welcome from './pages/welcome'
+import Game from './pages/game'
 
 const queryClient = new QueryClient()
 
@@ -20,13 +21,13 @@ export const App = () => {
               />
               <Route
                 path='/game'
-                element={<GridStoryLayout />}
+                element={<Game />}
                 //  TODO: Loading component to be created here
                 loader={() => <div>loading component here</div>}
               />
               <Route
                 path='/create'
-                element={<CreatePlayer />}
+                element={<CreatePlayerNew />}
                 //  TODO: Loading component to be created here
                 loader={() => <div>loading component here</div>}
               />
