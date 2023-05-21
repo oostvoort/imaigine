8e716ac5b372d932e71aac55ccd245518a5d3a02
Lorenz Dela Fuente <lorenz@oostvoort.com>
2023-05-21 14:49:36 +0800

    chore: removed mock up on PostDeploy.s.sol

--- packages/contracts/script/PostDeploy.s.sol
+++ packages/contracts/script/PostDeploy.s.sol
@@ -19,50 +19,6 @@ contract PostDeploy is Script {
 //    uint32 newValue = IWorld(worldAddress).increment();
 //    console.log("Increment via IWorld:", newValue);
 
-    IWorld world = IWorld(worldAddress);
-
-    string memory json = vm.readFile("sample_world.json");
-
-    // Player name, race
-    string memory name = vm.parseJsonString(json, ".locations.[0].characters.[0].name");
-    //    string memory race = vm.parseJsonString(json, ".locations.[0].characters.[0].race");
-    string memory race = "human";
-
-    bytes32 playerID = world.createPlayer(name, race);
-
-    // Attributes
-    string[] memory attrNames = new string[](6);
-    attrNames[0] = "strength";
-    attrNames[1] = "dexterity";
-    attrNames[2] = "constitution";
-    attrNames[3] = "intelligence";
-    attrNames[4] = "charisma";
-    attrNames[5] = "wisdom";
-
-    uint256[] memory attrValues = new uint256[](6);
-    attrValues[0] = vm.parseJsonUint(json, ".locations.[0].characters.[0].stats.strength");
-    attrValues[1] = vm.parseJsonUint(json, ".locations.[0].characters.[0].stats.dexterity");
-    attrValues[2] = vm.parseJsonUint(json, ".locations.[0].characters.[0].stats.constitution");
-    attrValues[3] = vm.parseJsonUint(json, ".locations.[0].characters.[0].stats.intelligence");
-    attrValues[4] = vm.parseJsonUint(json, ".locations.[0].characters.[0].stats.charisma");
-    attrValues[5] = vm.parseJsonUint(json, ".locations.[0].characters.[0].stats.wisdom");
-
-    world.setAttributesUint(playerID, attrNames, attrValues);
-
-    string memory location = vm.parseJsonString(json, ".locations.[0].name");
-    world.setLocation(playerID, location);
-
-    bytes32[] memory actionIDs = new bytes32[](2);
-    bytes[] memory actions = new bytes[](2);
-
-    actionIDs[0] = keccak256(abi.encode("options.locations.0"));
-    actions[0] = "Valley of Iron";
-
-    actionIDs[1] = keccak256(abi.encode("options.locations.1"));
-    actions[1] = "Central Town";
-
-    world.setPlayerStory(playerID, actionIDs, actions);
-
     vm.stopBroadcast();
   }
 }
