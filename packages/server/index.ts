111e881b21c8796d08a9d0359e0ef5877cbde1d6
Caspar Oostendorp <caspar@oostvoort.com>
2023-05-22 07:40:26 +0800

    test: adding basic smoketests for the api

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,62 +1,62 @@
-import * as dotenv from 'dotenv';
-import cors from 'cors';
-import express, {Request, Response} from 'express';
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import express, { Request, Response } from 'express'
 import {
-    CharacterStats,
-    CharacterStory,
-    GeneratePlayerCharacterProps,
-    GenerateLocationProps,
-    GenerateStoryProps,
-    GenerateNonPlayerCharacterProps
+  CharacterStats,
+  CharacterStory,
+  GeneratePlayerCharacterProps,
+  GenerateLocationProps,
+  GenerateStoryProps,
+  GenerateNonPlayerCharacterProps,
 } from 'types'
-import {generateStory} from "./lib/openai/generate/generateStory";
-import {generateLocation} from "./lib/openai/generate/generateLocation";
-import {generateNonPlayerCharacter, generatePlayerCharacter} from "./lib/openai/generate/generateCharacter";
-import {generatePlayerImage} from "./lib/leonardo";
+import { generateStory } from './lib/openai/generate/generateStory'
+import { generateLocation } from './lib/openai/generate/generateLocation'
+import { generateNonPlayerCharacter, generatePlayerCharacter } from './lib/openai/generate/generateCharacter'
+import { generatePlayerImage } from './lib/leonardo'
 
-dotenv.config();
-const app = express();
-const port = 3000;
+dotenv.config()
+const app = express()
+const port = 3000
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use(
-    cors({
-        origin: '*',
-    })
-);
+  cors({
+    origin: '*',
+  }),
+)
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('OK 👌');
-});
+  res.send('OK 👌')
+})
 
 app.post('/generateStory', async (req: Request, res: Response) => {
-    const props: GenerateStoryProps = req.body
-    res.send(await generateStory(props));
-});
+  const props: GenerateStoryProps = req.body
+  res.send(await generateStory(props))
+})
 
 app.post('/generateLocation', async (req: Request, res: Response) => {
-    const props: GenerateLocationProps = req.body
-    res.send(await generateLocation(props));
-});
+  const props: GenerateLocationProps = req.body
+  res.send(await generateLocation(props))
+})
 
 app.post('/generatePlayerCharacter', async (req: Request, res: Response) => {
-    const props: GeneratePlayerCharacterProps = req.body
+  const props: GeneratePlayerCharacterProps = req.body
 
-    const player = await generatePlayerCharacter(props);
+  const player = await generatePlayerCharacter(props)
 
-    // player.imageHash = await generatePlayerImage(player.visualSummary)
+  player.imageHash = await generatePlayerImage(player.visualSummary)
 
-    res.send(player);
-});
+  res.send(player)
+})
 
 app.post('/generateNonPlayerCharacter', async (req: Request, res: Response) => {
-    const props: GenerateNonPlayerCharacterProps = req.body
-    res.send(await generateNonPlayerCharacter(props));
-});
+  const props: GenerateNonPlayerCharacterProps = req.body
+  res.send(await generateNonPlayerCharacter(props))
+})
 
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
