9138e08a8d07cc47d1b4837b60af32fc185e2492
Caspar Oostendorp <caspar@oostvoort.com>
2023-05-23 21:33:42 +0800

    fix: prompt

--- packages/server/lib/openai/generate/generateInteraction.ts
+++ packages/server/lib/openai/generate/generateInteraction.ts
@@ -67,7 +67,7 @@ export async function generateInteraction({
     Give me exactly 3 future actions ${orSpoken} by ${activeEntity.name}, with their possible effects on karma, encoded in the json
     The actions can not have speech. The dialog has to be as if  ${activeEntity.name} speaks.
     Karma effect are numbers between -5 and 5, depending on whether the action is negative or good for the world and ${activeEntity.name}'s conscience .
-
+    Summarize the interaction in one sentence.
     ${PROMPT_OUTPUT_JSON}.
     ${JSON.stringify(outputTemplate)}
     `
