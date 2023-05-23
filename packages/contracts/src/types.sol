6ab68d891acf8452859976dabee0ed0933df03d9
Lorenz Dela Fuente <lorenz@oostvoort.com>
2023-05-23 16:35:51 +0800

    refactor: simplify ActionData struct

--- packages/contracts/src/types.sol
+++ packages/contracts/src/types.sol
@@ -2,13 +2,9 @@
 pragma solidity >=0.8.0;
 
 library Types {
-  struct ActionEffect {
-    int256 karmaChange;
-  }
-
   struct ActionData {
     string mode;
     string content;
-    ActionEffect effects;
+    int256 karmaChange;
   }
 }
