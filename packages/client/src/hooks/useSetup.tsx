9070f025651120c4e1243229f4c4beabe5346176
Lance Terrence Magsombol <lance@oostvoort.com>
2023-05-23 15:31:09 +0800

    fix: add incomplete createCharacter function

--- packages/client/src/hooks/useSetup.tsx
+++ packages/client/src/hooks/useSetup.tsx
@@ -7,7 +7,8 @@ export default function useSetup() {
     systemCalls: {
       createStory,
       createStartingLocation,
-      createPlayer
+      createPlayer,
+      createCharacter
     }
   } = useMUD()
 
