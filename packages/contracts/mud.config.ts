8e96828d3d1ff5dbd83a52dcc887b24746ad296c
mariz <marrise@oostvoort.com>
2023-06-21 16:57:59 +0800

    refactor: contract to only store config ipfs hash

--- packages/contracts/mud.config.ts
+++ packages/contracts/mud.config.ts
@@ -4,13 +4,8 @@ export default mudConfig({
     systems: {
     },
     tables: {
-        StoryComponent: {
-            schema: {
-                themeID: "bytes32", // keccak256(theme)
-                currencyID: "bytes32", // The currency
-                racesID: "bytes", // Store available races to occur on story
-            },
-        },
+        StoryComponent: "bool",
+        ConfigComponent: "string",
         ItemComponent: "bool",
         OwnerComponent: "bytes32",
         AliveComponent: "bool",
