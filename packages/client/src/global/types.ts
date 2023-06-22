e25d4af648403f005fdd39823fe9bf8ed71ed73a
John Mark <john@oostvoort.com>
2023-06-22 10:01:48 +0800

    refactor(global): change global types

--- packages/client/src/global/types.ts
+++ packages/client/src/global/types.ts
@@ -1,8 +1,30 @@
-export type UserInputType = {
+import {PromiseOrValue} from "contracts/types/ethers-contracts/common";
+
+export interface CreateStoryTypes {
+    name: PromiseOrValue<string>,
+    summary: PromiseOrValue<string>,
+    theme: PromiseOrValue<string>,
+    races: Array<PromiseOrValue<string>>
+    currency: PromiseOrValue<string>
+}
+
+export interface CreateLocationTypes {
+   name: PromiseOrValue<string>,
+   summary: PromiseOrValue<string>,
+   imgHash: PromiseOrValue<string>
+}
+
+export interface GeneratePlayerProps {
   ageGroup: string,
   genderIdentity: string,
-  color: string,
   race: string,
   skinColor: string,
   bodyType: string,
+  favColor: string
+}
+
+export interface GeneratePlayerResponse {
+  ipfsHash: string
+  imgHashes: string[],
+  locationId: string
 }
