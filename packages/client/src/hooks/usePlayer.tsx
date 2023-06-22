33cbd5c56cfc818ac1c7ac2ec169cb7a0c684def
John Mark <john@oostvoort.com>
2023-06-22 10:03:07 +0800

    feat(api): add usePlayer Hooks

---
+++ packages/client/src/hooks/usePlayer.tsx
@@ -0,0 +1,71 @@
+import {useMUD} from '@/MUDContext';
+import {useComponentValue} from "@latticexyz/react"
+import {useMutation} from "react-query";
+import { IPFS_URL_PREFIX, SERVER_API } from '@/global/constant'
+import {GeneratePlayer} from "./api/generate/typesGenerate";
+import {awaitStreamValue} from "@latticexyz/utils";
+import {GeneratePlayerProps, GeneratePlayerResponse} from '@/global/types';
+
+
+export default function usePlayer () {
+    const {
+        components: {
+            CharacterComponent,
+            AliveComponent,
+            ImageComponent,
+            LocationComponent,
+            InteractComponent,
+        },
+
+        network: {
+            worldSend,
+            txReduced$,
+            playerEntity
+        },
+    } = useMUD();
+
+    const player = {
+        character: useComponentValue(CharacterComponent, playerEntity),
+        alive: useComponentValue(AliveComponent, playerEntity),
+        imgHash: useComponentValue(ImageComponent, playerEntity),
+        location: useComponentValue(LocationComponent, playerEntity),
+        interact: useComponentValue(InteractComponent, playerEntity),
+    }
+
+    const generatePlayer = useMutation<Awaited<GeneratePlayerResponse>, Error, GeneratePlayerProps>(async (data) => {
+        const response = await fetch(`${SERVER_API}/api/v1/generate-player`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+
+        const responseData = await response.json() as GeneratePlayerResponse;
+
+        const getDataFromIPFS = await fetch(`${IPFS_URL_PREFIX}/${responseData.ipfsHash}`, {
+          method: "GET",
+        });
+
+        const ipfsData = await getDataFromIPFS.json();
+
+        return { ...responseData, ...ipfsData };
+    }, {
+        mutationKey: ["generatePlayer"],
+    })
+
+    const createPlayer = useMutation<typeof player, Error, GeneratePlayer>(async (data) => {
+        const { config, imgHash, locationId } = data;
+        const tx = await worldSend("createPlayer", [config, imgHash, locationId])
+        await awaitStreamValue(txReduced$ , (txHash) => txHash === tx.hash)
+        return player
+    }, {
+        mutationKey: ["createPlayer"],
+    })
+
+    return {
+        player,
+        generatePlayer,
+        createPlayer,
+    }
+}
