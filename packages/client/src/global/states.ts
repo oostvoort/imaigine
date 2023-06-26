8d8926054ce3fcf5a67d0302530d6f65f926f60c
David Valdez <david@oostvoort.com>
2023-06-26 09:15:27 +0800

    chore: add route for create new character

--- packages/client/src/global/states.ts
+++ packages/client/src/global/states.ts
@@ -4,7 +4,7 @@ export const activeScreen_atom = atom<
   'startingScreen' |
   'createAvatarScreen' |
   'currentLocationScreen'
->('createAvatarScreen')
+>('startingScreen')
 
 export const currentLoader_atom = atom<
   'loadingAvatar' |
