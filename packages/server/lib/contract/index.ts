f5e8d4fb9e0f49f9b8fedd028cf5dab2f8e2f9ec
mariz <marrise@oostvoort.com>
2023-06-28 09:59:03 +0800

    fix: server not running

--- packages/server/lib/contract/index.ts
+++ packages/server/lib/contract/index.ts
@@ -3,13 +3,22 @@ import { providers, Wallet } from 'ethers'
 import worldsJson from "../../../contracts/worlds.json";
 const worlds = worldsJson as Partial<Record<string, { address: string; blockNumber?: number }>>;
 
+if (!process.env.PRIVATE_KEY) throw new Error('No private key provided')
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const JSON_RPC_URL = process.env.JSON_RPC_URL ?? 'http://127.0.0.1:8545'
+const CHAIN_ID = process.env.CHAIN_ID ?? '31337'
+
+console.info('Setting up Contract')
+console.info('JSON_RPC_URL:', JSON_RPC_URL)
+console.info('CHAIN_ID:', CHAIN_ID)
+
 // create a signer
 export const signer = new Wallet(
-  process.env.PRIVATE_KEY,
+  PRIVATE_KEY,
   new providers.JsonRpcProvider(
-    process.env.JSON_RPC_URL
+    JSON_RPC_URL
   )
 )
 // create a world contract object
 // this is already in typeScript
-export const worldContract = IWorld__factory.connect(worlds[process.env.CHAIN_ID].address, signer)
+export const worldContract = IWorld__factory.connect(worlds[CHAIN_ID].address, signer)
