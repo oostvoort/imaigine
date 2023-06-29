2142a823bc060e48499a13c59070707b304f90c5
Caspar Oostendorp <caspar@oostvoort.com>
2023-06-30 05:49:40 +0800

    working on custom map codes

--- packages/fantasy-map-generator/imaigine.js
+++ packages/fantasy-map-generator/imaigine.js
@@ -5,11 +5,13 @@ function hook_onMapLoaded (){
   console.log("Imaigine: hook_onMapLoaded")
   window.parent.postMessage("FinishedLoadingMap")
 
+  fogCells([558, 559, 560])
 }
+
 /**
- * Anything we want to run after the map was loaded.
+ * Anything we want to run after the map was clicked.
  */
-function hook_onMapClick (el, p, cellId){
+function hook_onMapClick (el, p){
   console.log("Imaigine: hook_onMapClick", el, p)
 
   const parent = el.parentElement;
@@ -29,4 +31,16 @@ function hook_onMapClick (el, p, cellId){
       name: burgElement.textContent
     })
   }
+}
+
+function fogCells(cells){
+  console.log("fogCells", cells)
+  const getPathPoints = cells => cells.map(i => (Array.isArray(i) ? i : burg[i] ? getBurgCoords(burg[i]) : p[i]));
+  const getPath = segment => round(lineGen(getPathPoints(segment)), 1);
+  fog("myFogId", cells)
+
+}
+
+function unfogCells(id){
+  unfog("myFogId")
 }
