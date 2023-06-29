c0b640f1594f0edca81d8922a5296d688b152bb1
mariz <marrise@oostvoort.com>
2023-06-29 13:11:50 +0800

    chore: stub the methods

--- packages/contracts/src/systems/TravelSystem.sol
+++ packages/contracts/src/systems/TravelSystem.sol
@@ -18,18 +18,22 @@ contract TravelSystem is System {
 
   /// @dev called by the player to signal that the player would like to travel
   /// @param locationId pertains to the cell number of the destination
-  /// @preturn the locationId in bytes32 form
+  /// @return the locationId in bytes32 form
   function prepareTravel(uint256 cellNumber) public returns (bytes32) {
     bytes32 locationId = keccak256(abi.encodePacked(bytes16("LOCATION"), cellNumber));
-    require(SceneComponent.get(locationId), "location does not exist");
-
-    bytes32 playerID = bytes32(uint256(uint160(_msgSender())));
-
-    // making it so that the player is not interacting with anything
-    InteractableComponent.set(playerID, new bytes(0));
-    TravelComponent.set(playerID, TravelStatus.)
-
-
     return locationId;
+  }
+
+  /// @dev called by the backend to process the player's travel
+  /// @param playerId is the player that will start travelling
+  /// @param cellNumbers is a list of cells the player will travel through
+  function startTravel(bytes32 playerId, uint256[] memory cellNumbers) public {
+
+  }
+
+  /// @dev called by the player to update the player's current location
+  /// @returns the current cell number the player is on
+  function travel() public returns (uint256) {
+
   }
 }
