451fe05f1a74bdc725f901f2f73d02a5edfcd127
John Mark <john@oostvoort.com>
2023-07-07 13:50:31 +0800

    refactor(type): battle and betting types

--- packages/client/src/hooks/minigame/types/battle.ts
+++ packages/client/src/hooks/minigame/types/battle.ts
@@ -1,7 +1,5 @@
 import { PromiseOrValue } from 'contracts/types/ethers-contracts/common'
 
 export type SETBATTLETYPES = {
-  playerId: PromiseOrValue<string>,
-  locationId: PromiseOrValue<string>,
   opponentId: PromiseOrValue<string>
 }
--- packages/client/src/hooks/minigame/types/betting.ts
+++ packages/client/src/hooks/minigame/types/betting.ts
@@ -1,7 +1,6 @@
 import { PromiseOrValue } from 'contracts/types/ethers-contracts/common'
 
 export type SETBETTINGTYPES = {
-  playerId: PromiseOrValue<string>,
-  locationId: PromiseOrValue<string>,
-  hashOption: PromiseOrValue<string>
+  playerId: PromiseOrValue<string> | string,
+  locationId: PromiseOrValue<string> | string
 }
