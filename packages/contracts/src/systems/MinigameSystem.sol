1e096e281e214527ad12955b6a7c5394aa9de160
John Mark <john@oostvoort.com>
2023-07-07 13:51:50 +0800

    refactor(docs_feat): add docstring and feat

--- packages/contracts/src/systems/MinigameSystem.sol
+++ packages/contracts/src/systems/MinigameSystem.sol
@@ -13,11 +13,23 @@ import { BattleStatus } from "../codegen/Types.sol";
 
 contract MinigameSystem is System {
 
+    struct HashOptionsValue {
+        string memory key;
+        string memory data;
+        uint256 timestamp;
+    }
+
     /// This function sets a player in the battle queue for a specific location.
-    /// It first checks if the player is already in the queue by calling the "get" function from the BattleQueueComponent contract and
-    /// then comparing the result to 0. If the player is not already in the queue,
-    /// the function sets the player in the queue by calling the "set" function from the BattleQueueComponent contract with the given
-    /// locationId and playerId. Finally, it returns the playerId of the player that was just added to the queue.
+    /// It first checks if the player is already in the queue by calling the "get" function from the BattleQueueComponent contract and then comparing the result to 0.
+    /// If the player is not already in the queue, the function sets the player in the queue by calling the "set" function from the BattleQueueComponent contract with the given locationId and playerId.
+    /// Finally, it returns the playerId of the player that was just added to the queue.
+
+    /**
+     * Sets a player in the queue for a battle at a location.
+     * @param playerId - The ID of the player to add to the queue.
+     * @param locationId - The ID of the location to add the player to the queue for.
+     * @return The player ID added to the queue.
+     */
     function setQueue(bytes32 playerId, bytes32 locationId) public returns (bytes32) {
 
         require(BattleQueueComponent.get(locationId) == 0, "player is already in the queue");
@@ -27,11 +39,20 @@ contract MinigameSystem is System {
         return BattleQueueComponent.get(locationId);
     }
 
+
     /// This function sets up a battle match between two players. It takes in the IDs of the player, location, and opponent as parameters.
     /// First, it checks if the player is already in a battle by verifying their status. If the player is already in a battle, it throws an error.
     /// Next, it sets the battle component data for the player and location. It assigns the opponent ID, sets the status to "IN_BATTLE", and initializes the option and hashSalt values.
     /// Then, it deletes the record from the battle queue component for the specified location.
     /// Finally, it returns the battle component data for the player and location.
+
+    /**
+    * Sets up a battle match between two players.
+    * @param playerId - The ID of the player to add to the match.
+    * @param locationId - The ID of the location to add the player to the match for.
+    * @param opponentId - The ID of the opponent to match the player against.
+    * @return The battle component data for the player in the match.
+    */
     function setMatch(bytes32 playerId, bytes32 locationId, bytes32 opponentId) public returns (BattleComponentData memory) {
 
         require(BattleComponent.get(playerId, locationId).status != BattleStatus.IN_BATTLE, "player is already in the battle");
@@ -40,7 +61,11 @@ contract MinigameSystem is System {
             opponent: opponentId,
             status: BattleStatus.IN_BATTLE,
             option: "",
-            hashSalt: ""
+            hashSalt: HashOptionsValue({
+                key: "",
+                data: "",
+                timestamp: 0
+            })
         }));
 
         BattleQueueComponent.deleteRecord(locationId);
@@ -51,9 +76,17 @@ contract MinigameSystem is System {
 
     /// This function sets the betting option for a player in a battle.
     /// It takes in the player ID, location ID, and a hashed option as parameters.
-    /// It first checks if the player exists in the match by verifying their status. If the player exists,
-    /// it updates their betting option with the provided hash. Finally,
-    /// it returns the updated battle component data for the player in the specified location.
+    /// It first checks if the player exists in the match by verifying their status.
+    /// If the player exists, it updates their betting option with the provided hash.
+    /// Finally, it returns the updated battle component data for the player in the specified location.
+
+    /**
+    * Sets a player's betting option for a battle.
+    * @param playerId - The ID of the player to set the betting option for.
+    * @param locationId - The ID of the location the player is in a battle at.
+    * @param hashOption - The hashed betting option to set for the player.
+    * @return The updated battle component data for the player.
+    */
     function setBetting(bytes32 playerId, bytes32 locationId, bytes32 hashOption) public returns (BattleComponentData memory) {
 
       require(BattleComponent.get(playerId, locationId).status == BattleStatus.IN_BATTLE, "player doesn't exist in the match");
@@ -61,5 +94,20 @@ contract MinigameSystem is System {
       BattleComponent.setOption(playerId, locationId, hashOption);
 
       return BattleComponent.get(playerId, locationId);
+    }
+
+
+    // TODO
+    function lockInBetting(bytes32 playerId, bytes32 locationId, string[] memory _hashsalt) public {
+        require(_hashsalt.length == 3, "invalid hash salt");
+        require(BattleComponent.get(playerId, locationId).status == BattleStatus.IN_BATTLE, "player doesn't exist in the match");
+
+        HashOptionsValue memory value = HashOptionsValue({
+            key : _hashsalt[0],
+            data : _hashsalt[1],
+            timestamp : _hashsalt[2]
+        });
+
+        BattleComponent.pushHashSalt(playerId, locationId, value);
     }
 }
