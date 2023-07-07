51e46f4b0ab944c59bd2c61f258be54ec2240634
mariz <marrise@oostvoort.com>
2023-07-07 14:52:47 +0800

    chore: put in name and legend

--- packages/contracts/src/lib/BitMapLib.sol
+++ packages/contracts/src/lib/BitMapLib.sol
@@ -3,8 +3,8 @@ pragma solidity >=0.8.0;
 
 library BitMapLib {
   function setRevealedCell(uint256[] memory self, uint256 bitIndex, bool revealed) internal {
-    uint256 arrayRow = bitIndex / 256;
-    uint256 arrayColumn = bitIndex % 256;
+    uint256 arrayRow = bitIndex / 1024;
+    uint256 arrayColumn = bitIndex % 1024;
     self[arrayRow] = (self[arrayRow] & ~(1 << arrayColumn)) | (uint256(revealed ? 1 : 0) << arrayColumn);
   }
 
