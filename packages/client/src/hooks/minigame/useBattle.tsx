3996834ffde12adfd3da147f9d3da2c71b2b5677
John Mark <john@oostvoort.com>
2023-07-10 15:54:08 +0800

    docs: comment for every function

--- packages/client/src/hooks/minigame/useBattle.tsx
+++ packages/client/src/hooks/minigame/useBattle.tsx
@@ -29,6 +29,16 @@ export default function useBattle(playerId: Entity) {
     battle: useComponentValue(BattleComponent, playerId),
   }
 
+  /**
+   * Generates a hash of the selected battle options.
+   * @param options The selected battle options.
+   * Generates a key hash from the string "SECRET_ID".
+   * Generates a data hash from the selected options.
+   * Gets the current timestamp.
+   * Generates a hash of the key, data and timestamp using solidityKeccak256.
+   * Sets the battleOption state to the generated hash.
+   * Sets the hash_options_set_value atom to an object containing the key, data and timestamp.
+   */
   const onSelectOptions = (options: string) => {
     const key = utils.keccak256(utils.toUtf8Bytes("SECRET_ID"))
     const data = utils.keccak256(utils.toUtf8Bytes(options))
@@ -40,6 +50,13 @@ export default function useBattle(playerId: Entity) {
     setHashAtom({ key, data, timestamp })
   }
 
+  /**
+   * Defines a mutation hook to set battle options.
+   * @throws Error if battleOption is undefined.
+   * Sends a transaction with the battle options.
+   * Waits for the transaction to be confirmed.
+   * Returns the battle data.
+   */
   const setBattle = useMutation({
     mutationKey: ["setBattle"],
     mutationFn: async () => {
@@ -52,6 +69,16 @@ export default function useBattle(playerId: Entity) {
     }
   })
 
+  /**
+   * Defines a mutation hook to lock in battle options.
+   * @param data An object containing the hashSalt and options to lock in.
+   * @param hashSalt The hash salt corresponding to the selected options.
+   * @param options The selected battle options.
+   * @throws Error if hashSalt or options are undefined.
+   * Sends a transaction to lock in the battle options.
+   * Waits for the transaction to be confirmed.
+   * Returns the battle data.
+   */
   const lockIn = useMutation({
     mutationKey: ["lockIn"],
     mutationFn: async (data: LOCKINTYPES) => {
