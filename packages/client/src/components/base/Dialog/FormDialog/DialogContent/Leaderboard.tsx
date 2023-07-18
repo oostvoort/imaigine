62712001af96ce297c42f1e120fa64eaec151751
renzo <rnzdcn@gmail.com>
2023-07-18 14:03:54 +0800

    fix: sort by battle points of player

--- packages/client/src/components/base/Dialog/FormDialog/DialogContent/Leaderboard.tsx
+++ packages/client/src/components/base/Dialog/FormDialog/DialogContent/Leaderboard.tsx
@@ -8,13 +8,15 @@ export default function Leaderboard() {
   const { player } = usePlayer()
   const { getAllPlayersBattlePoints } = useHistory(player.id as Entity)
 
+  const sortedPlayers = getAllPlayersBattlePoints.map(({data}) => data).sort((a, b) => Number(b?.playerBP.value) - Number(a?.playerBP.value)).slice(0, 4)
+
   return (
     <div className={clsx([ 'flex flex-col min-w-[478px] w-full' ])}>
       <p
         className={clsx([ 'text-accent text-base', 'uppercase font-jost font-medium tracking-[1.4px]' ])}>Leaderboard</p>
       <ul>
         {
-           getAllPlayersBattlePoints.map(({ data }, index) => {
+          sortedPlayers.map((data, index) => {
 
               return (
                 <li
@@ -22,13 +24,13 @@ export default function Leaderboard() {
                   className={clsx([ 'flex items-center', 'text-[16px] text-white', 'font-bold font-segoe tracking-[0.2px]', 'mt-sm' ])}>
 
                   <div
-                    className={clsx([ 'basis-10', 'w-[38px] h-[38px]', 'rounded-full', 'flex justify-center items-center', 'bg-[#2C3B47]' ])}>{index + 1}
+                    className={clsx([{'bg-[#E6B415]': index === 0}, {'bg-[#A5A5A5]': index === 1}, {'bg-[#AF5200]': index === 2}, 'basis-10', 'w-[38px] h-[38px]', 'rounded-full', 'flex justify-center items-center', 'bg-[#2C3B47]' ])}>{index + 1}
                   </div>
 
                   <p
                     className={clsx([ 'flex-1', 'ml-sm', 'text-[20px] leading-[32px] text-left text-option-11', 'font-segoe tracking-[0.4px]' ])}>{data?.playerInfo.name}</p>
                   <p
-                    className={clsx([ 'basis-4/12', 'text-[16px] leading-[32px] text-right text-option-11', 'font-segoe tracking-[0.32px]' ])}>{Number(data?.playerBP.value)}
+                    className={clsx([ 'basis-4/12', 'text-[16px] leading-[32px] text-right text-option-11', 'font-segoe tracking-[0.32px]' ])}>{`${Number(data?.playerBP.value)} BP` }
                   </p>
                 </li>
               )
