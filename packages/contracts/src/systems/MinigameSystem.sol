9e18c62be6101fe1212f1e5914d2e28af8903fa9
John Mark <john@oostvoort.com>
2023-08-03 11:22:21 +0800

    fix: adding checks before deleting history

--- packages/contracts/src/systems/MinigameSystem.sol
+++ packages/contracts/src/systems/MinigameSystem.sol
@@ -113,9 +113,13 @@ contract MinigameSystem is System {
   // @dev removes BattleHistory Records based on battleHistoryIds
   // @note call from front-end and passed an ids array
   function removeBattleLogs(uint256[] memory battleHistoryIds) public {
-    for (uint256 i = 0; i < battleHistoryIds.length; i++) {
-      BattleHistoryComponent.deleteRecord(battleHistoryIds[i]);
+
+    if (battleHistoryIds.length > 0) {
+      for (uint256 i = 0; i < battleHistoryIds.length; i++) {
+        BattleHistoryComponent.deleteRecord(battleHistoryIds[i]);
+      }
     }
+
   }
 
   // @dev Can only be called after deadline
