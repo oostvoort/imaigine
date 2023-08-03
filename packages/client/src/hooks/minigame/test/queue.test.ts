e3c677ae7f5a07603868175394426e62a11d2bd9
John Mark <john@oostvoort.com>
2023-08-03 14:17:05 +0800

    chore: delete unused front-end hooks test files

--- packages/client/src/hooks/minigame/test/queue.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, it } from '@jest/globals'
-import useQueue from '@/hooks/minigame/useQueue'
-
-it('test_returns_object_with_battle_queue_and_set_queue_properties', () => {
-  const result = useQueue();
-  expect(result).toHaveProperty('battleQueue');
-  expect(result).toHaveProperty('setQueue');
-});
